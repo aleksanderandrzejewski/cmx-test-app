6d19a9d0800c721e2deb306ac5ff9c76440a82a3
aleksanderandrzejewski <18704749+aleksanderandrzejewski@users.noreply.github.com>
2022-09-21 22:04:26 +0000

    passing instance name as an argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,37 @@
 #include <ctime>
 #include <iostream>
+#include <string>
 #include <cmw-cmx-cpp/ProcessComponent.h>
 #include <cmw-cmx-cpp/Component.h>
 
 using namespace cmw::cmx;
 
-int main()
+int main(int argc, char *argv[])
 {
+    std::string instance_name;
+    if (argc > 1)
+    {
+        instance_name = argv[1];
+    } else {
+        instance_name = "cmx_test_app";
+    }
+
     ProcessComponent::update();
 
-    ComponentPtr component = Component::create("stats");
-    CmxInt64 metr_test = component->newInt64("test");
+    ComponentPtr component = Component::create(instance_name);
+    CmxInt64 test_counter = component->newInt64("test_counter");
 
     std::cout << "Enter loop" << std::endl;
-    int i=0;
+    int i = 0;
     while (true)
     {
         for (int i = 0; i < 100; i++)
         {
-            metr_test = i;  // update metric
-            ProcessComponent::update(); // update process metrics
+            test_counter = i;
+            ProcessComponent::update();
             usleep(1000000);
-            if (i % 10 == 0) std::cout << i << std::endl;
+            if (i % 10 == 0)
+                std::cout << i << std::endl;
         }
     }
 }
