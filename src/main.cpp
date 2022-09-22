2cc4d133347c7745cc3ab9dfa8723072f3d68ef1
aleksanderandrzejewski <18704749+aleksanderandrzejewski@users.noreply.github.com>
2022-09-22 07:08:47 +0000

    test_gauge1: random steps up and down

--- src/main.cpp
+++ src/main.cpp
@@ -22,10 +22,12 @@ int main(int argc, char *argv[])
     std::random_device dev;
     std::mt19937 rng(dev());
     std::uniform_int_distribution<std::mt19937::result_type> distr(1,10);
+    std::uniform_int_distribution<std::mt19937::result_type> distr2(-5,5);
 
     ComponentPtr component = Component::create(instance_name);
     CmxInt64 test_counter1 = component->newInt64("test_counter1");
     CmxInt64 test_counter2 = component->newInt64("test_counter2");
+    CmxInt64 test_gauge1 = component->newInt64("test_gauge1");
 
     std::cout << "Enter loop" << std::endl;
     int i = 0;
@@ -35,6 +37,13 @@ int main(int argc, char *argv[])
         {
             test_counter1 = i;
             test_counter2 = test_counter2 + distr(rng);
+
+            int test_gauge1_tmp = test_gauge1 + distr2(rng);
+            if (test_gauge1_tmp <= 100 and test_gauge1_tmp>=0)
+            {
+                test_gauge1 = test_gauge1_tmp;
+            }
+
             ProcessComponent::update();
             
             if (i % 10 == 0)
